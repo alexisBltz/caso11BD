3069c4a432c7ae428dadb0447ebc77ad4a18d00e
alexisBltz <almamanima@unsa.edu.pe>
2024-06-17 18:26:30 -0500

    FINISH

---
+++ src/main/java/org/app/TipoTrabajador.java
@@ -0,0 +1,31 @@
+package org.app;
+
+public class TipoTrabajador {
+    private char trabajadortipoTraId;
+    private String tibtrabDesc;
+    private char TrabEstReg;
+
+    public char getTrabajadortipoTraId() {
+        return trabajadortipoTraId;
+    }
+
+    public void setTrabajadortipoTraId(char trabajadortipoTraId) {
+        this.trabajadortipoTraId = trabajadortipoTraId;
+    }
+
+    public String getTibtrabDesc() {
+        return tibtrabDesc;
+    }
+
+    public void setTibtrabDesc(String tibtrabDesc) {
+        this.tibtrabDesc = tibtrabDesc;
+    }
+
+    public char getTrabEstReg() {
+        return TrabEstReg;
+    }
+
+    public void setTrabEstReg(char trabEstReg) {
+        TrabEstReg = trabEstReg;
+    }
+}
