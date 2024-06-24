40b2a7a12fbed41bf3280bd6ca8c5cccdc6ef816
alexisBltz <almamanima@unsa.edu.pe>
2024-06-24 00:22:50 -0500

    Haciendo el formulario para Tipo de Cuenta Corriente

--- src/main/java/org/app/dao/TipoCuentaCorrienteDAO.java
+++ src/main/java/org/app/dao/TipoCuentaCorrienteDAO.java
@@ -1,4 +1,102 @@
 package org.app.dao;
 
+import org.app.DatabaseConnection;
+import org.app.controllers.TipoCuentaCorriente;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class TipoCuentaCorrienteDAO {
+    public void createTipoCuentaCorriente(TipoCuentaCorriente tipoCuentaCorriente) {
+        String sql = "INSERT INTO tb_tipocuentacorriente (TipoCuentaCorrienteCod, TipCueCorNom, TipCueCorEstReg) VALUES (?,?,?)";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, tipoCuentaCorriente.getTipoCuentaCorrienteCod());
+            pstmt.setString(2, tipoCuentaCorriente.getTipCueCorNom());
+            pstmt.setString(3, String.valueOf(tipoCuentaCorriente.getTipCueCorEstReg()));
+            pstmt.executeUpdate();
+            System.out.println("Tipo de cuenta corriente creado exitosamente!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<TipoCuentaCorriente> readTiposCuentaCorriente() {
+        List<TipoCuentaCorriente> tiposCuentaCorriente = new ArrayList<>();
+        String sql = "SELECT * FROM tb_tipocuentacorriente";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) {
+
+            while (rs.next()) {
+                TipoCuentaCorriente tipoCuentaCorriente = new TipoCuentaCorriente();
+                tipoCuentaCorriente.setTipoCuentaCorrienteCod(rs.getInt("TipoCuentaCorrienteCod"));
+                tipoCuentaCorriente.setTipCueCorNom(rs.getString("TipCueCorNom"));
+                tipoCuentaCorriente.setTipCueCorEstReg(rs.getString("TipCueCorEstReg").charAt(0));
+                tiposCuentaCorriente.add(tipoCuentaCorriente);
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("Error al leer el registro de tipos de cuenta corriente");
+        }
+        return tiposCuentaCorriente;
+    }
+
+    public void updateTipoCuentaCorriente(TipoCuentaCorriente tipoCuentaCorriente) {
+        String sql = "UPDATE tb_tipocuentacorriente SET TipCueCorNom = ?, TipCueCorEstReg = ?  WHERE TipoCuentaCorrienteCod = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setString(1, tipoCuentaCorriente.getTipCueCorNom());
+            pstmt.setString(2, String.valueOf(tipoCuentaCorriente.getTipCueCorEstReg()));
+            pstmt.setInt(3, tipoCuentaCorriente.getTipoCuentaCorrienteCod());
+            pstmt.executeUpdate();
+            System.out.println("Tipo de cuenta corriente actualizado exitosamente!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void deleteTipoCuentaCorriente(int tipoCuentaCorrienteCod) {
+        String sql = "DELETE FROM tb_tipocuentacorriente WHERE TipoCuentaCorrienteCod = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, tipoCuentaCorrienteCod);
+            pstmt.executeUpdate();
+            System.out.println("Tipo de cuenta corriente eliminado exitosamente!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void inactivarTipoCuentaCorriente(int tipoCuentaCorrienteCod) {
+        String sql = "UPDATE tb_tipocuentacorriente SET TipCueCorEstReg = 'I' WHERE TipoCuentaCorrienteCod = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, tipoCuentaCorrienteCod);
+            pstmt.executeUpdate();
+            System.out.println("Tipo de cuenta corriente inactivado exitosamente!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void activarTipoCuentaCorriente(int tipoCuentaCorrienteCod) {
+        String sql = "UPDATE tb_tipocuentacorriente SET TipCueCorEstReg = 'A' WHERE TipoCuentaCorrienteCod = ?";
+
+        try (Connection connection = DatabaseConnection.getConnection();
+             PreparedStatement pstmt = connection.prepareStatement(sql)) {
+            pstmt.setInt(1, tipoCuentaCorrienteCod);
+            pstmt.executeUpdate();
+            System.out.println("Tipo de cuenta corriente activado exitosamente!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/main/java/org/app/forms/TipoCuentaCorrienteForm.java
+++ src/main/java/org/app/forms/TipoCuentaCorrienteForm.java
@@ -1,4 +1,165 @@
 package org.app.forms;
 
-public class TipoCuentaCorrienteForm {
+import org.app.controllers.TipoCuentaCorriente;
+import org.app.dao.TipoCuentaCorrienteDAO;
+import org.app.forms.ModelosFormularios.TablasReferencialesBase;
+
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.List;
+
+public class TipoCuentaCorrienteForm extends TablasReferencialesBase {
+    private TipoCuentaCorrienteDAO tipoCuentaCorrienteDAO;
+
+    public TipoCuentaCorrienteForm(JFrame parent) {
+        super(parent);
+        setTitle("Tipo de Cuenta Corriente");
+        NombreTabla.setText("Tipo de Cuenta Corriente");
+
+        tipoCuentaCorrienteDAO = new TipoCuentaCorrienteDAO();
+
+        agregarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                TipoCuentaCorriente tipoCuentaCorriente = createTipoCuentaCorriente();
+
+                tipoCuentaCorrienteDAO.createTipoCuentaCorriente(tipoCuentaCorriente);
+                limpiarCampos();
+                cargarLista();
+            }
+        });
+
+        modificarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                update();
+                limpiarCampos();
+                cargarLista();
+            }
+        });
+
+        eliminarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tipoCuentaCorrienteDAO.deleteTipoCuentaCorriente(Integer.parseInt(ID.getText()));
+                limpiarCampos();
+                cargarLista();
+            }
+        });
+
+        inactivarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tipoCuentaCorrienteDAO.inactivarTipoCuentaCorriente(Integer.parseInt(ID.getText()));
+                EstadoRegistro.setText("I");
+                cargarLista();
+            }
+        });
+
+        activarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                tipoCuentaCorrienteDAO.activarTipoCuentaCorriente(Integer.parseInt(ID.getText()));
+                EstadoRegistro.setText("A");
+                cargarLista();
+            }
+        });
+
+        cancelarButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                limpiarCampos();
+            }
+        });
+
+        salirButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+        results.getSelectionModel().addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                if (!e.getValueIsAdjusting() && results.getSelectedRow() != -1) {
+                    int selectedRow = results.getSelectedRow();
+                    ID.setText(results.getValueAt(selectedRow, 0).toString());
+                    Descripcion.setText(results.getValueAt(selectedRow, 1).toString());
+                    EstadoRegistro.setText(results.getValueAt(selectedRow, 2).toString());
+                    ID.setEnabled(false);
+                }
+            }
+        });
+
+        setVisible(true);
+    }
+
+    @Override
+    protected void createUIComponents() {
+        tipoCuentaCorrienteDAO = new TipoCuentaCorrienteDAO();
+        List<TipoCuentaCorriente> tiposCuentaCorriente = tipoCuentaCorrienteDAO.readTiposCuentaCorriente();
+
+        DefaultTableModel modelDefault = new DefaultTableModel();
+        modelDefault.setColumnIdentifiers(new String[]{
+                "ID", "Nombre", "Estado de Registro"
+        });
+
+        for (TipoCuentaCorriente tipoCuentaCorriente : tiposCuentaCorriente) {
+            Object[] rowData = {
+                    tipoCuentaCorriente.getTipoCuentaCorrienteCod(),
+                    tipoCuentaCorriente.getTipCueCorNom(),
+                    tipoCuentaCorriente.getTipCueCorEstReg(),
+            };
+            modelDefault.addRow(rowData);
+        }
+
+        results = new JTable(modelDefault);
+    }
+
+    private void update() {
+        TipoCuentaCorriente tipoCuentaCorriente = createTipoCuentaCorriente();
+        tipoCuentaCorrienteDAO.updateTipoCuentaCorriente(tipoCuentaCorriente);
+        limpiarCampos();
+        cargarLista();
+    }
+
+    private void cargarLista() {
+        List<TipoCuentaCorriente> lista = tipoCuentaCorrienteDAO.readTiposCuentaCorriente();
+
+        DefaultTableModel modelo = (DefaultTableModel) results.getModel();
+        modelo.getDataVector().clear();
+
+        for (TipoCuentaCorriente tipoCuentaCorriente : lista) {
+            Object[] data = {
+                    tipoCuentaCorriente.getTipoCuentaCorrienteCod(),
+                    tipoCuentaCorriente.getTipCueCorNom(),
+                    tipoCuentaCorriente.getTipCueCorEstReg(),
+            };
+            modelo.addRow(data);
+        }
+    }
+
+    private TipoCuentaCorriente createTipoCuentaCorriente() {
+        TipoCuentaCorriente tipoCuentaCorriente = new TipoCuentaCorriente();
+        tipoCuentaCorriente.setTipoCuentaCorrienteCod(Integer.parseInt(ID.getText()));
+        tipoCuentaCorriente.setTipCueCorNom(Descripcion.getText());
+        tipoCuentaCorriente.setTipCueCorEstReg(EstadoRegistro.getText().charAt(0));
+        return tipoCuentaCorriente;
+    }
+
+    private void limpiarCampos() {
+        ID.setText("");
+        Descripcion.setText("");
+        EstadoRegistro.setText("");
+    }
+
+    public static void main(String[] args) {
+        TipoCuentaCorrienteForm tipoCuentaCorrienteForm = new TipoCuentaCorrienteForm(null);
+    }
 }
+
